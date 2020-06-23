4f1becedf18f6ba2dfe205b1ef0a070ba5a6f52b
Jim Barrows <Jim.Barrows@gmail.com>
2020-06-23 15:44:01 -0500

    Iniital

---
+++ src/main/java/org/erpmicroservices/shipment/endpoints/rest/ShipmentEndpointRestApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.shipment.endpoints.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentEndpointRestApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(ShipmentEndpointRestApplication.class, args);
+ }
+
+}
---
+++ src/test/java/org/erpmicroservices/shipment/endpoints/rest/shipmentendpointrest/ShipmentEndpointRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.shipment.endpoints.rest.shipmentendpointrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentEndpointRestApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
